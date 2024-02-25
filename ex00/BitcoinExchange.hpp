49c9cee6132620d2939a05cbe0eaaddcbfb16bc1
leebo155 <leebohyeong155@gmail.com>
2024-02-25 18:22:06 +0900

    ex00 commit

--- ex00/BitcoinExchange.hpp
+++ ex00/BitcoinExchange.hpp
@@ -6,19 +6,33 @@
 /*   By: bohlee <bohlee@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/23 18:34:13 by bohlee            #+#    #+#             */
-/*   Updated: 2024/02/23 19:03:37 by bohlee           ###   ########.fr       */
+/*   Updated: 2024/02/25 18:21:43 by bohlee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef BITCOINEXCHANGE_HPP
 # define BITCOINEXCHANGE_HPP
 
+# include <iostream>
+# include <exception>
+# include <fstream>
+# include <ctime>
+# include <cstdlib>
+# include <map>
+
 class BitcoinExchange
 {
 	private:
-		std::map<std::string, int>	database;
+		std::map<std::string, double>	database;
+
+		std::string	trim(const std::string &line);
+		struct tm	makeTime(const std::string &line);
+		std::string checkDate(const std::string &line) throw(int);
+		double		checkPrice(const std::string &line) throw(int);
+		std::map<std::string, double>::iterator findDate(const std::string &date);
 
 	public:
+
 		BitcoinExchange(const char *csvfile) throw(std::exception);
 		BitcoinExchange(void);
 		BitcoinExchange(const BitcoinExchange &target);
@@ -26,6 +40,7 @@ class BitcoinExchange
 		~BitcoinExchange(void);
 
 		void	calculate(const char *inputfile) throw(std::exception);
+		void	setDatabase(const char *csvfile) throw(std::exception);
 };
 
 #endif
---
+++ ex00/main.cpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bohlee <bohlee@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/24 17:14:33 by bohlee            #+#    #+#             */
+/*   Updated: 2024/02/25 18:18:33 by bohlee           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "BitcoinExchange.hpp"
+
+int main(int argc, char *argv[])
+{
+	try {
+		if (argc - 1 != 1)
+			throw std::runtime_error("could not open file.");
+
+		BitcoinExchange test("data.csv");
+
+		test.calculate(argv[1]);
+
+	} catch (std::exception &e) {
+		std::cerr << "Error: " << e.what() << std::endl;
+	}
+	system("leaks btc");
+	return 0;
+}
