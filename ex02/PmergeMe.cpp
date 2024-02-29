dae7d5ff383a4f43fb35b4abdb8e9da6a8974d39
leebo155 <leebohyeong155@gmail.com>
2024-02-29 20:44:14 +0900

    sorting...

--- ex02/PmergeMe.cpp
+++ ex02/PmergeMe.cpp
@@ -6,7 +6,7 @@
 /*   By: bohlee <bohlee@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/27 18:17:48 by bohlee            #+#    #+#             */
-/*   Updated: 2024/02/28 20:10:29 by bohlee           ###   ########.fr       */
+/*   Updated: 2024/02/29 20:42:27 by bohlee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,26 +22,6 @@ PmergeMe::~PmergeMe(void)
 {
 	vt.clear();
 	lt.clear();
-}
-
-void	PmergeMe::splitMerge(int a, int b, int node_size, T &t)
-{
-	if (b  - a < node_size)
-		return ;
-	else if (b - a == node_size )
-	{
-		if (t.at(a) < t.at(b))
-		{
-			t.insert(t.begin() + a,
-					t.begin() + b, t.begin() + b + node_size);
-			t.erase(t.begin() + b + node_size + 1,
-					t.begin() b + node_size * 2 + 1);
-		}
-		return ;
-	}
-	int size = (b - a) / 2;
-	this->splitMerge(a, size, t);
-	this->splitMerge(a + size + 1, end, t);	
 }
 
 void	PmergeMe::sort(int argc, char **argv) throw(std::exception)
@@ -61,25 +41,26 @@ void	PmergeMe::sort(int argc, char **argv) throw(std::exception)
 			lt.push_back(num);
 		}
 	}
-	std::cout << "\nvector: ";
-	for (std::vector<uint64_t>::iterator it = vt.begin(); it != vt.end(); it++)
-		std::cout << *it << " ";
-	std::cout << std::endl;
-	std::cout << "list: ";
-	for (std::list<uint64_t>::iterator it = lt.begin(); it != lt.end(); it++)
-		std::cout << *it << " ";
-	std::cout << std::endl;	
-
-	for (int i = 1; i <= (argc - 1) / 2; i * 2)
-		this->splitMerge(0, (argc - 1) / i * i, i, vt);
-
 	std::cout << "vector: ";
 	for (std::vector<uint64_t>::iterator it = vt.begin(); it != vt.end(); it++)
 		std::cout << *it << " ";
 	std::cout << std::endl;
-	std::cout << "list: ";
-	for (std::list<uint64_t>::iterator it = lt.begin(); it != lt.end(); it++)
-		std::cout << *it << " ";
-	std::cout << std::endl;	
+//	std::cout << "list: ";
+//	for (std::list<uint64_t>::iterator it = lt.begin(); it != lt.end(); it++)
+//		std::cout << *it << " ";
+//	std::cout << std::endl;	
+
+	this->splitPair(argc - 1, vt);
+	for (int i = 2; i <= (argc - 1) / 2; i += i)
+		this->mergeRecur(0, (argc - 1) / (i * 2) * (i * 2) - 1, i,  vt);
+
+//	std::cout << "\nvector: ";
+//	for (std::vector<uint64_t>::iterator it = vt.begin(); it != vt.end(); it++)
+//		std::cout << *it << " ";
+//	std::cout << std::endl;
+//	std::cout << "list: ";
+//	for (std::list<uint64_t>::iterator it = lt.begin(); it != lt.end(); it++)
+//		std::cout << *it << " ";
+//	std::cout << std::endl;	
 }
 
--- ex02/PmergeMe.hpp
+++ ex02/PmergeMe.hpp
@@ -6,7 +6,7 @@
 /*   By: bohlee <bohlee@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/27 17:55:39 by bohlee            #+#    #+#             */
-/*   Updated: 2024/02/28 19:41:24 by bohlee           ###   ########.fr       */
+/*   Updated: 2024/02/29 20:43:10 by bohlee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,8 +25,55 @@ class PmergeMe
 		std::vector<uint64_t> vt;
 		std::list<uint64_t> lt;
 
-		template <typename T>;
-		void	splitMerge(
+		template <typename T>
+		void	splitPair(int size, T &t)
+		{
+			for (int i = 0; i + 1 < size; i += 2)
+			{
+				int a = i;
+				int b = i + 1;
+				if (t.at(a) < t.at(b))
+				{
+					T	tmp;
+					tmp.insert(tmp.begin(), t.begin() + b, t.begin() + b + 1);
+					t.erase(t.begin() + b);
+					t.insert(t.begin() + a, tmp.begin(), tmp.end());
+					tmp.clear();
+					std::cout << "vector: ";
+					for (typename T::iterator it = t.begin(); it != t.end(); it++)
+						std::cout << *it << " ";
+					std::cout << std::endl;
+				}
+			}
+		};
+		template <typename T>
+		void	mergeRecur(int a, int b, int node_size, T &t)
+		{
+			int size = (b - a) / 2;
+			if (b  - a + 1 < node_size )
+				return ;
+			else if (b - a + 1 == node_size * 2)
+			{
+				b = a + size + 1;
+				if (t.at(a) < t.at(b))
+				{
+					T	tmp;
+					tmp.insert(tmp.begin(), t.begin() + b, t.begin() + b + node_size);
+					t.erase(t.begin() + b, t.begin() + b + node_size);
+					std::cout << std::endl;
+					t.insert(t.begin() + a, tmp.begin(), tmp.end());
+					tmp.clear();
+					std::cout << "vector: ";
+					for (typename T::iterator it = t.begin(); it != t.end(); it++)
+						std::cout << *it << " ";
+					std::cout << std::endl;
+				}
+				return ;
+			}
+
+			this->mergeRecur(a, size, node_size, t);
+			this->mergeRecur(a + size + 1, b, node_size, t);	
+		};
 
 	public:
 		PmergeMe(void);
